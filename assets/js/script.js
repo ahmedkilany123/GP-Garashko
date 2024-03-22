960621bf7660152728ccc223cc93f24ab3237bc7
ahmedkilany123 <97148391+ahmedkilany123@users.noreply.github.com>
2024-03-22 22:13:22 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 /**
  * element toggle function
  */
 
-const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
 /**
  * navbar toggle
@@ -24,7 +24,9 @@ const navElemArr = [overlay, navCloseBtn, navOpenBtn];
  * close navbar when click on any navbar link
  */
 
-for (let i = 0; i < navbarLinks.length; i++) { navElemArr.push(navbarLinks[i]); }
+for (let i = 0; i < navbarLinks.length; i++) {
+  navElemArr.push(navbarLinks[i]);
+}
 
 /**
  * addd event on all elements for toggling navbar
@@ -37,7 +39,28 @@ for (let i = 0; i < navElemArr.length; i++) {
   });
 }
 
+let saerchform = document.querySelector(".search-form");
+document.querySelector("#search-btn").onclick = () => {
+  saerchform.classList.toggle("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
 
+let shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+  saerchform.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+let loginForm = document.querySelector(".login-form");
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+  saerchform.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  navbar.classList.remove("active");
+};
 
 /**
  * header active state
@@ -46,6 +69,7 @@ for (let i = 0; i < navElemArr.length; i++) {
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", function () {
-  window.scrollY >= 400 ? header.classList.add("active")
+  window.scrollY >= 400
+    ? header.classList.add("active")
     : header.classList.remove("active");
-}); 
+});
